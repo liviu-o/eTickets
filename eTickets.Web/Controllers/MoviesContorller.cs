477bf51e8868e7ed345f6d4c74ac5f8526b844c6
LiviuCauia <li.viu@mail.com>
2022-12-12 13:28:51 +0000

    Add Controllers with index action

---
+++ eTickets.Web/Controllers/MoviesContorller.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Movies.ToListAsync();
+            return View(data);
+        }
+    }
+}
