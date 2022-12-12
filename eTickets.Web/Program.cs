0ea7c8afa3b2855e6cd12a6b0dbb4ac9b4bcce1d
LiviuCauia <li.viu@mail.com>
2022-12-12 10:20:42 +0000

    Configure Connection Strings and add Migration

--- eTickets.Web/Program.cs
+++ eTickets.Web/Program.cs
@@ -1,9 +1,12 @@
 using eTickets.Web.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnectionString")
+)); 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
